3e229e8f1e9c0a599dd0f68b72a65d8622d764eb
Stefan Plazic <stefan.plazi@gmail.com>
2023-11-18 11:03:10 +0100

    Enable cors

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,8 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   app.setGlobalPrefix("api");
 
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
